781f6d842cd3162a52513449d6b7e765a5c021e4
Diegoni <romain_diegoni@hotmail.com>
2017-06-13 22:20:53 +0100

    Setup dynamic style as unique to avoid to interfere to the other when the component is used several time in the same apge

--- lib/components/Stars.jsx
+++ lib/components/Stars.jsx
@@ -17,19 +17,28 @@ export default class Stars extends React.Component {
         super(props);
 
         this.state = { value: props.value };
+        this.uniqueness = (Math.random() + '').replace('.', '');
+    }
+
+    mouseLeave(event) {
+        event.preventDefault();
+
+        if (event.clientX <= event.target.getBoundingClientRect().left) {
+            this.setValue(0);
+        } else if (event.clientX >= event.target.getBoundingClientRect().right) {
+            this.setValue(this.props.count);
+        }
     }
 
     mouseOver(event) {
-      const spread = event.target.getBoundingClientRect().right - event.target.getBoundingClientRect().left;
-      const mouseAt = event.clientX - event.target.getBoundingClientRect().left;
+        event.preventDefault();
 
-      if (mouseAt > 0) {
+        const spread = event.target.getBoundingClientRect().right - event.target.getBoundingClientRect().left;
+        const mouseAt = event.clientX - event.target.getBoundingClientRect().left;
+
+        if (mouseAt > 0) {
           let index = Number(event.target.getAttribute('data-index'));
-          this.setState({ value: index + (mouseAt / spread) });
-
-          if (this.props.onChange) {
-            this.props.onChange(index + (mouseAt / spread));
-          }
+          this.setValue(index + (mouseAt / spread));
       }
     }
 
@@ -42,10 +51,16 @@ export default class Stars extends React.Component {
         const mouseAt = event.clientX - event.target.getBoundingClientRect().left;
 
         let index = Number(event.target.getAttribute('data-index'));
-        this.setState({ value: index + (mouseAt / spread)});
+        this.setValue(index + (mouseAt / spread));
+    }
+
+    setValue(value) {
+        const roundValue = +value.toFixed(2);
+
+        this.setState({ value: roundValue });
 
         if (this.props.onChange) {
-          this.props.onChange(index + (mouseAt / spread));
+          this.props.onChange(roundValue);
         }
     }
 
@@ -63,24 +78,24 @@ export default class Stars extends React.Component {
 
     getStyleElement(width) {
         return `
-          .stars__star--partial:before {
-            display: inline-flex;
-            position: absolute;
-            overflow: hidden;
-            z-index: 10;
-            top: 0; left: 0;
-            width: ${width};
-            content: attr(data-forhalf);
-            color: #ffd700;
-        }
-        .stars__star--partial:after {
-          display: inline-flex;
-          overflow: hidden;
-          top: 0; left: 0;
-          width: 100%;
-          content: attr(data-forhalf);
-          color: #ccc;
-      }`;
+            .stars__star--${this.uniqueness}:before {
+                display: inline-flex;
+                position: absolute;
+                overflow: hidden;
+                z-index: 10;
+                top: 0; left: 0;
+                width: ${width};
+                content: attr(data-forhalf);
+                color: #ffd700;
+            }
+            .stars__star--${this.uniqueness}:after {
+                display: inline-flex;
+                overflow: hidden;
+                top: 0; left: 0;
+                width: 100%;
+                content: attr(data-forhalf);
+                color: #ccc;
+            }`;
     }
 
     renderStyleElement(width) {
@@ -94,16 +109,17 @@ export default class Stars extends React.Component {
     }
 
     renderStars() {
-        const { uniqueness, stars, value } = this.state;
+        console.log(this.state.value);
         return times(this.props.count, i => {
           const isActive = (this.state.value - (i + 1)) > 0 || this.state.value === this.props.count;
           const isPartial = (this.state.value - i) > 0 && (this.state.value - i) < 1;
-          const className = bem('stars__star', { 'active': isActive, 'partial': isPartial });
+          const className = bem('stars__star', { 'active': isActive, [this.uniqueness]: isPartial });
 
           return (
              <div key={i}
                 onMouseOver={this.props.edit && this.mouseOver.bind(this)}
                 onMouseMove={this.props.edit && this.mouseOver.bind(this)}
+                onMouseLeave={this.props.edit && this.mouseLeave.bind(this)}
                 onClick={this.props.edit && this.click.bind(this)}
                 className={className}
                 data-index={i}
